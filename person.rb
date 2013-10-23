af12d8056c3f897086c072bd66799260f4e56b0b
Le6ow5k1 <bebot@mail.ru>
2013-10-23 20:28:30 +0600

    add reader

--- person.rb
+++ person.rb
@@ -2,12 +2,20 @@
 class Person
 	attr_accessor :age, :salary, :height, :weight
 
-	# Если параметры отсутсвуют, объект иницилизируется со слючайными значениями параметров
-	def initialize age=rand(0..100), salary=rand(0..1000000), height=rand(0..200), weight=rand(0..200)
+	def initialize age=rand(0..100), salary=rand(0..1000000.0), height=rand(0..200), weight=rand(0..200)
 		@age = age
 		@salary = salary
 		@height = height
 		@weight = weight
+	end
+
+	def field_value(field)
+		case field
+		when :age 	 then self.age
+		when :salary then self.salary
+		when :height then self.height
+		when :weight then self.weight
+		end
 	end
 
 end
