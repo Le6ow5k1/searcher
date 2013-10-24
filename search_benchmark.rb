a95212662c046e38b3aee3ae06ac12606fffbd1f
Le6ow5k1 <bebot@mail.ru>
2013-10-24 12:28:13 +0600

    add more tests

--- search_benchmark.rb
+++ search_benchmark.rb
@@ -12,17 +12,13 @@ for i in 1..N
 end
 @criteria = Hash[:age => 99..100, :salary => 1000.0..800000.0, :height => 200, :weight => (1..200)]
 
-Benchmark.bmbm do |rep| 
+Benchmark.bmbm do |rep|
   # rep.report("load:   ") { @searcher.load(@testData) }
-  rep.report("parse_criteria: ") {@searcher.parse_criteria(@criteria)}
-  rep.report("selectivity: ") { @searcher.selectivity(:salary, (0..1000000.0)) }
-  # rep.report("for i in []: ") { res=[]
-  # 															for i in @testData do
-  # 																if not((80..100)===i.field_value(:age))and not((80..100)===i.field_value(:height))and not((80..100)===i.field_value(:height))
-  # 																	res << i
-  # 																end
-  # 															end }
+  # rep.report("parse_criteria: ") {@searcher.parse_criteria(@criteria)}
+  # rep.report("selectivity: ") { @searcher.selectivity(:salary, (0..1000000.0)) }
   # rep.report("keep_if: ") {@testData.keep_if{|i| (80..100)===i.field_value(:age)}}
-  rep.report("scan_search: ") { @searcher.scan_search(@testData.dup, @criteria) }
-  rep.report("search: ") { @searcher.search(@testData.dup, @criteria) }
+  # rep.report("scan_search: ") { @searcher.scan_search(@testData.dup, @criteria) }
+  # rep.report("search: ") { @searcher.search(@testData.dup, @criteria) }
+  rep.report("add_indexes: ") { @searcher.add_indexes(@testData, [:age, :salary, :height, :weight]) }
+  rep.report("search_with_index: ") { @searcher.search_with_index(@testData, @criteria) }
 end
