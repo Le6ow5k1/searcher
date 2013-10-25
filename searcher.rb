30b5067201ca9716c5ee5655a5a62c0e4f4c4644
Le6ow5k1 <bebot@mail.ru>
2013-10-26 00:45:31 +0600

    fix

--- searcher.rb
+++ searcher.rb
@@ -6,12 +6,12 @@ class Searcher
 
 	def load(data)
 		@data = data
-		# add_indexes([:age, :height, :weight, :salary])
+		add_indexes([:age, :height, :weight])
 	end
 
 	# Создает хэш индексов по заданным полям
 	def add_indexes(fields)
-		indexes = fields.map {|field| [field, build_index(field)]}.flatten
+		indexes = fields.flat_map {|field| [field, build_index(field)]}
 		@indexes = Hash[*indexes]
 	end
 
@@ -43,11 +43,17 @@ class Searcher
 
   # Расчитывает селективность определенного условия
   def selectivity(field, range)
+  	diff = if (range.end - range.begin) != 0
+  		(range.end - range.begin)
+  	else
+  		0.01
+  	end
+
   	case field
-  	when :age 	 then (range.end - range.begin) / 100.0
-		when :salary then (range.end - range.begin) / 1000000.0
-		when :height then (range.end - range.begin) / 200.0
-		when :weight then (range.end - range.begin) / 200.0
+  	when :age 	 then diff / 100.0
+		when :salary then diff / 1000000.0
+		when :height then diff / 200.0
+		when :weight then diff / 200.0
 		end
   end
 
@@ -64,13 +70,16 @@ class Searcher
 		crit = parse_criteria(criteria)
 		# Проходим по условиям в порядке их селективности
 		# и последовательно уменьшаем нашу выборку
-		crit.inject(@data.dup) do |data, criterion|
-			data.keep_if{|obj| (criterion[1]===obj.send(criterion[0]))}
+		crit.inject(@data) do |data, criterion|
+			break if data.empty?
+			data.keep_if{|obj| (criterion[1] === obj.send(criterion[0]))}
 		end
 	end
 
 	# Поиск с простым проходом по всем объектам
 	def scan_search(criteria)
+		raise Exception.new('Expecting a Hash') unless criteria.is_a? Hash
+
 		crit = parse_criteria(criteria)
 		result = []
 		for i in @data do
@@ -82,23 +91,38 @@ class Searcher
 	end
 
 	def search_with_index(criteria)
+		raise Exception.new('Expecting a Hash') unless criteria.is_a? Hash
+
 		crit = parse_criteria(criteria)
+		return scan_search(crit) if crit.size == 1 and crit[:salary]
+		salary = crit.delete(:salary)
 		first_field = crit.keys[0]
-		first_result = select_from_index(first_field, crit[first_field])
+		acc = select_from_index(first_field, crit[first_field])
+		return @data.values_at(*acc) if crit.size == 1
 		crit.delete(first_field)
-		crit.each_pair do |f, r|
-			first_result &= select_from_index(f, r)
+		crit.each do |f, r|
+			acc &= select_from_index(f, r)
 		end
 		result = []
-		for i in first_result do
+		for i in acc do
+			if salary.cover? @data[i].salary
 			result << @data[i]
+		end
 		end
 		result
 	end
 
+	def retrieve_from_index(field, range)
+    result = []
+    for i in @indexes[field]
+    	if range === i[0]
+    		result += @data.values_at(*i[1])
+    	end
+    end
+   	result
+	end
+
 	def select_from_index(field, range)
-		return [] unless field
-    return @indexes[field].keys unless range
     result = []
     for i in @indexes[field]
     	if range === i[0]
@@ -107,6 +131,5 @@ class Searcher
     end
    	result
 	end
-
 
 end
