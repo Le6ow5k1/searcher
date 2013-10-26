dff962451f42e23158d3ac177de07a80f46072b5
Le6ow5k1 <bebot@mail.ru>
2013-10-27 01:15:46 +0600

    add spec

--- spec/searcher_spec.rb
+++ spec/searcher_spec.rb
@@ -76,29 +76,38 @@ describe Searcher do
 	  end
 	end
 
-	describe 'search_with_index' do
+	describe 'kdtree_search' do
 		it 'returns objects that satisfy given criteria' do
 			searcher = Searcher.new
 			searcher.load(@people)
-			criteria = Hash[:age => (0..50), :salary => (100.0..1000000.0), :height => (0..200), :weight => (90..190)]
-	  	searcher.search_with_index(criteria).should be == [@p2, @p4]
+			criteria = Hash[:age => (0..150), :salary => (2000..1000000.0), :height => (100..190), :weight => (0..200)]
+	  	searcher.scan_search(criteria).should be == [@p2, @p5]
 	  end
 	end
 
-	describe 'build_index' do
-		it 'return index hash for given field' do
-			searcher = Searcher.new
-			searcher.load(@people)
-			searcher.build_index(:height).should be == {0 => [5], 100 => [4], 200 => [3], 150 => [0,2], 180 => [1]}
-		end
-	end
+	# describe 'search_with_index' do
+	# 	it 'returns objects that satisfy given criteria' do
+	# 		searcher = Searcher.new
+	# 		searcher.load(@people)
+	# 		criteria = Hash[:age => (0..50), :salary => (100.0..1000000.0), :height => (0..200), :weight => (90..190)]
+	#   	searcher.search_with_index(criteria).should be == [@p2, @p4]
+	#   end
+	# end
 
-	describe 'retrieve_from_index' do
-		it 'returns all obj with field values that are in given range' do
-			searcher = Searcher.new
-			searcher.load(@people)
-			searcher.retrieve_from_index(:age, 5..15).should be == [@p1, @p2]
-		end
-	end
+	# describe 'build_index' do
+	# 	it 'return index hash for given field' do
+	# 		searcher = Searcher.new
+	# 		searcher.load(@people)
+	# 		searcher.build_index(:height).should be == {0 => [5], 100 => [4], 200 => [3], 150 => [0,2], 180 => [1]}
+	# 	end
+	# end
+
+	# describe 'retrieve_from_index' do
+	# 	it 'returns all obj with field values that are in given range' do
+	# 		searcher = Searcher.new
+	# 		searcher.load(@people)
+	# 		searcher.retrieve_from_index(:age, 5..15).should be == [@p1, @p2]
+	# 	end
+	# end
 
 end
