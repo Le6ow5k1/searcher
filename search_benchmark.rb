7139ced89a6a555a1fc34403e4626552f49b6666
Le6ow5k1 <bebot@mail.ru>
2013-10-28 01:57:43 +0600

    prettify

--- search_benchmark.rb
+++ search_benchmark.rb
@@ -10,16 +10,17 @@ N = 10_000_000
 for i in 1..N
   @testData << Person.new
 end
-@criteria = Hash[:age => 99..100, :salary => 1000.0..80000.0, :height => 200, :weight => (1..200)]
-
-Benchmark.bmbm do |rep|
-  # rep.report("load:   ") { @searcher.load(@testData) }
-  # rep.report("parse_criteria: ") {@searcher.parse_criteria(@criteria)}
-  # rep.report("selectivity: ") { @searcher.selectivity(:salary, (0..1000000.0)) }
-  # rep.report("keep_if: ") {@testData.keep_if{|i| (80..100)===i.field_value(:age)}}
-  # rep.report("scan_search: ") { @searcher.scan_search(@testData.dup, @criteria) }
-  # rep.report("search: ") { @searcher.search(@testData.dup, @criteria) }
-  rep.report("add_indexes: ") { @searcher.add_indexes(@testData, [:salary]) }
-  rep.report("select: ") { @searcher.select_from_index(:salary, 10000..1000000.0) }
-  # rep.report("search_with_index: ") { @searcher.search_with_index(@testData, @criteria) }
+@criteria = []
+@criteria << Hash[:age => (80..150), :salary => (20000..1000000.0), :height => (120..190), :weight => (0..200)]
+@criteria << Hash[:age => 69..100, :salary => 20000.0..80000.0, :height => 150..200, :weight => 110..200]
+@criteria << Hash[:age => 89..100, :height => 180..200, :weight => 40..110]
+@criteria << Hash[:height => 10..20, :weight => 160..200]
+@criteria << Hash[:salary => 80000.0..200000.0]
+@criteria.each do |c|
+  Benchmark.bmbm do |rep|
+    rep.report("load:   ") { @searcher.load(@testData) }
+    rep.report("kdtree_search: ") { @searcher.kdtree_search(c.dup) }
+    rep.report("selectivity_search: ") { @searcher.selectivity_search(c.dup) }
+    rep.report("scan_search: ") { @searcher.scan_search(c.dup) }
+  end
 end
