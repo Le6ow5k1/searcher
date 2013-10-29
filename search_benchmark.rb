ab382c8af157930fbf4b06253c4ebdf35f83bcf7
Le6ow5k1 <bebot@mail.ru>
2013-10-30 00:22:49 +0600

    fix

--- search_benchmark.rb
+++ search_benchmark.rb
@@ -11,12 +11,14 @@ for i in 1..N
   @testData << Person.new
 end
 @criteria = []
-@criteria << Hash[:age => (80..150), :salary => (20000..1000000.0), :height => (120..190), :weight => (0..200)]
-@criteria << Hash[:age => 69..100, :salary => 20000.0..80000.0, :height => 150..200, :weight => 110..200]
-@criteria << Hash[:age => 89..100, :height => 180..200, :weight => 40..110]
-@criteria << Hash[:height => 10..20, :weight => 160..200]
-@criteria << Hash[:salary => 80000.0..200000.0]
+@criteria << Hash[:age => (30..100), :salary => (90000..1000000.0), :height => (120..190), :weight => (50..200)]
+@criteria << Hash[:age => (60..100), :salary => (200000..1000000.0), :height => (150..190), :weight => (90..200)]
+@criteria << Hash[:age => (89..100), :height => (180..200), :weight => (40..110)]
+@criteria << Hash[:height => (10..20), :weight => (160..200)]
+@criteria << Hash[:salary=>80000.0..200000.0]
+
 @criteria.each do |c|
+  p c
   Benchmark.bmbm do |rep|
     rep.report("load:   ") { @searcher.load(@testData) }
     rep.report("kdtree_search: ") { @searcher.kdtree_search(c.dup) }
